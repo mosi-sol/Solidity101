6cb507116c4ef9f52282a01ce9a8f137e9a08e81
mosi <99150661+mosi-sol@users.noreply.github.com>
2023-03-18 18:09:36 +0330

    Add files via upload

---
+++ collection-1/Foundry Test/forge-test.bat
@@ -0,0 +1,23 @@
+::wsl
+@echo off
+title Forge-unit test %DATE%
+
+CALL :more
+pause
+CALL :run
+EXIT /B %ERRORLEVEL% 
+
+:run
+echo ready to run, press any key
+pause
+forge test -vvv > what.txt
+echo %DATE% >> what.txt
+echo %TIME% >> what.txt
+echo result save on : what.txt
+pause
+forge test --gas-report
+pause
+EXIT /B 0
+
+:more
+MORE what.txt
